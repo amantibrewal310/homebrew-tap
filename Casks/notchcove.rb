e6f7e4ce529a52ff127c10caebfe739c8cfdf9e8
Aman Tibrewal <amantibrewal310@gmail.com>
2026-09-24 18:38:07 +0530

    notchcove 0.1.2

--- Casks/notchcove.rb
+++ Casks/notchcove.rb
@@ -1,6 +1,6 @@
 cask "notchcove" do
-  version "0.1.1"
-  sha256 "8e9cbd27ced719166a2af9749a222450ff9570f8c2d7f92f72a1cf130b730bb9"
+  version "0.1.2"
+  sha256 "8691fe654821405c931952826537aba5564b41484e33f63a5a8385afb06860ee"
 
   url "https://github.com/amantibrewal310/NotchCove/releases/download/v#{version}/NotchCove-#{version}.zip"
   name "NotchCove"
